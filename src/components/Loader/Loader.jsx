662fe13ad676504430628be41ae3ba6bdd6dcc9d
Dobrobatko Dmytro <dobrobatka@gmail.com>
2023-04-09 19:53:45 +0300

    add update

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,27 @@
+import { FallingLines } from 'react-loader-spinner';
+
+const Loader = () => {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0,
+        background: 'rgba(0, 0, 0, 0.6)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <FallingLines
+        color="#4fa94d"
+        width="100"
+        visible={true}
+        ariaLabel="falling-lines-loading"
+      />
+    </div>
+  );
+};
+export default Loader;
