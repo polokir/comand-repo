575daf8cecf8e8692453c41b63da053da027a628
AlexanderShevchenko112 <aleks.shevchenko.2013@gmail.com>
2023-04-09 19:24:11 +0300

    TASK1 IS DONE

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,25 +3,26 @@ import { Component, useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
-export const SearchForm =({handleSubmit})=> {
-    const [query,setQuery] = useState('');
-    const onSubmitHandler = (e) =>{
-      e.preventDefault();
-      handleSubmit(query)
-    }
-    return (
-      <SearchFormStyled onSubmit={onSubmitHandler} >
+export const SearchForm = ({ handleSubmit }) => {
+  const [query, setQuery] = useState('');
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    handleSubmit(query);
+    setQuery('');
+  };
+  return (
+    <SearchFormStyled onSubmit={onSubmitHandler}>
       <FormBtn type="submit">
         <FiSearch size="16px" />
       </FormBtn>
       <InputSearch
         value={query}
-        onChange={e=>setQuery(e.target.value)}
+        onChange={e => setQuery(e.target.value)}
         placeholder="What do you want to write?"
         name="search"
         required
         autoFocus
       />
-    </SearchFormStyled>);
-  
-}
+    </SearchFormStyled>
+  );
+};
