90a735ff30f7efa073e5b6e077ebb1698c52a096
OlgaMunaieva <asyssync@gmail.com>
2023-04-09 20:51:10 +0300

    task3

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import 'modern-normalize';
 
 import { App } from 'components';
 import { GlobalStyles, theme } from 'styles';
+import testContext from 'context/testContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyles} />
-      <App />
+      <testContext.Provider value={{ text: 'first-context' }}>
+        <App />
+      </testContext.Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,39 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
-export class Todos extends Component {
-  render() {
-    return <Text>Todos</Text>;
-  }
-}
+export const Todos = () => {
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem('todos')) || []
+  );
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+  const onSubmit = text => {
+    console.log(text);
+    const todo = { id: nanoid(), text };
+    setTodos(prev => [...prev, todo]);
+  };
+
+  const deleteTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <>
+      <SearchForm handleSubmit={onSubmit} />
+      <Grid>
+        {todos.map((todo, index) => (
+          <GridItem key={todo.id}>
+            <Todo
+              id={todo.id}
+              text={todo.text}
+              counter={index + 1}
+              deleteTodo={deleteTodo}
+            />
+          </GridItem>
+        ))}
+      </Grid>
+    </>
+  );
+};
